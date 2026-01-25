8a1c7a9a30c3589208daa2993decae17d9c6ca88
1192to1682-code <1192to1682@gmail.com>
2026-01-25 12:55:23 +0900

    NPC01への挙動を設定した

--- Assets/Scripts/Field/NPCInteract.cs
+++ Assets/Scripts/Field/NPCInteract.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using UnityEngine.Events;
 public class NPCInteract : MonoBehaviour,IInteractable
 {
     public string NPCName = "村人A";
@@ -7,10 +7,20 @@ public class NPCInteract : MonoBehaviour,IInteractable
     [TextArea]
     public string TalkMessage = "こんにちは";
 
+    /// <summary>
+    /// 外からユニティーのイベント処理をする
+    /// </summary>
+    public UnityEvent NPCEvent;
+
     public void Interact()
     { 
     Debug.Log ($"[NPC]{TalkMessage}私は{NPCName}です");
-    
+        
+        //NPCEventが設定されていれば（nullじゃなかったら）,
+        //設定された処理を発動する。
+        NPCEvent?.Invoke();
+
+
     }
 
 
